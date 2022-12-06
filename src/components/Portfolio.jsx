a0f6d4a497e8acd49437f40f398cf0ba57c8bb80
Durlav Parajuli <durlove60@gmail.com>
2022-12-06 18:38:49 +0545

    portfolio-section-updated

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -68,10 +68,10 @@ const Portfolio = () => {
                 className="rounded-md duration-200 hover:scale-105"
               />
               <div className="flex items-center justify-center">
-                <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105" onClick={ () => window.open(link, '_blank')}>
-                  Demo
+                <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105" onClick={() => window.open(link, '_blank')}>
+                  Visit
                 </button>
-                <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105" onClick={ () => window.open(repo, '_blank')}>
+                <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105" onClick={() => window.open(repo, '_blank')}>
                   GitHub
                 </button>
               </div>
